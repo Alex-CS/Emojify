7f6a52857fa9b9efc3a621cb37ab4bb1d4fff2bb
Alex Simonides <acsimonides@gmail.com>
2015-04-26 00:36:21 -0500

    Reorganize directory structure

---
+++ server.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+#
+# Flask server
+#
+# Author: Alex Simonides
+
+from flask import Flask, render_template
+from flask.ext.restful import Api
+
+from emojify import EmojiTranslation
+
+app = Flask(__name__)
+
+api = Api(app)
+
+api.add_resource(EmojiTranslation, "/emojify")
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
